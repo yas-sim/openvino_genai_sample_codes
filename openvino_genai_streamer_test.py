b9ac15d5084a6c4adf42a21a5847ef7a4dd8882e
Yasunori Shimura <yasu0710@gmail.com>
2024-09-09 17:24:20 +0900

    removed comment line of optimum-cli

--- openvino_genai_streamer_test.py
+++ openvino_genai_streamer_test.py
@@ -1,6 +1,5 @@
 from ov_genai_utils.streamer import *
 
-# optimum-cli export openvino --trust-remote-code --model TinyLlama/TinyLlama-1.1B-Chat-v1.0 TinyLlama-1.1B-Chat-v1.0
 model_path = 'TinyLlama-1.1B-Chat-v1.0'
 
 ovs = OpenVINO_GenAI_Streamer(model_path, device='CPU', config={'CACHE_DIR':'./cache'})
